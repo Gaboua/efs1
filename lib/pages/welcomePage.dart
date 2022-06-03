afdb66417843926d40b4cbc5bce0abfd402936fb
Eya Arfa <arfaaya0@gmail.com>
2022-06-03 21:37:15 +0100

    test

--- lib/pages/welcomePage.dart
+++ lib/pages/welcomePage.dart
@@ -8,7 +8,7 @@ class WelcomePage extends StatefulWidget {
   State<WelcomePage> createState() => _WelcomePageState();
 }
 
-final Whitebackgroudheight = 0.6;
+final Whitebackgroudheight = 0.5;
 
 class _WelcomePageState extends State<WelcomePage> {
   @override
