7dc7b8d903f4cf5b314f454c6db3a25d498afa3f
Gaboua <sameh21gaboua@gmail.com>
2022-06-07 16:20:43 +0100

    verification finger_print.dart + fix Title position + add Single child scroll view to avoid keyboard problems + fix small height device problem in register.dart

--- lib/pages/otp.dart
+++ lib/pages/otp.dart
@@ -27,105 +27,111 @@ class _OtpState extends State<Otp> {
         actionf: () => Navigator.pop(context),
         titre: "Verification ",
       ),
-      body: SizedBox(
-        height: size.height,
-        width: size.width,
-        child: Stack(
-          children: [
-            Positioned(
-                bottom: 0,
-                child: Container(
-                    height: size.height * Whitebackgroudheight,
-                    width: size.width,
+      body: SingleChildScrollView(
+        child: SizedBox(
+          height: size.height,
+          width: size.width,
+          child: Stack(
+            children: [
+              Positioned(
+                  bottom: 0,
+                  child: Container(
+                      height: size.height * Whitebackgroudheight,
+                      width: size.width,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.only(
+                          topRight: Radius.circular(26),
+                        ),
+                      ))),
+              Positioned(
+                  bottom: size.height * Whitebackgroudheight - 1,
+                  child: Container(
+                    height: size.height * 0.05,
+                    width: size.width / 2,
                     decoration: BoxDecoration(
                       color: Colors.white,
-                      borderRadius: BorderRadius.only(
-                        topRight: Radius.circular(26),
-                      ),
-                    ))),
-            Positioned(
-                bottom: size.height * Whitebackgroudheight - 1,
-                child: Container(
-                  height: size.height * 0.05,
-                  width: size.width / 2,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                  ),
-                )),
-            Positioned(
-              bottom: size.height * Whitebackgroudheight,
-              child: Container(
-                  height: size.height * 0.05,
-                  width: size.width / 2,
-                  decoration: BoxDecoration(
-                      color: Color(0xff120879),
-                      borderRadius: BorderRadius.only(
-                        bottomLeft: Radius.circular(26),
-                      ))),
-            ),
-            Positioned(
-              bottom: size.height * (Whitebackgroudheight + 0.025),
-              left: (size.width -
-                      size.height * (1 - Whitebackgroudheight - 0.15)) /
-                  2,
-              child: Image(
-                width: size.height * (1 - Whitebackgroudheight - 0.15),
-                height: size.height * (1 - Whitebackgroudheight - 0.15),
-                image: AssetImage("assets/verifcode.PNG"),
-              ),
-            ),
-            Positioned(
-              bottom: size.height * (Whitebackgroudheight - 0.1),
-              left: size.width / 12,
-              child: Text(
-                "Verification Code",
-                style: TextStyle(fontSize: 30),
-              ),
-            ),
-            Positioned(
-              bottom: size.height / 3,
-              width: size.width / 1.2,
-              left: size.width / 12,
-              child: Form(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    textInput("****", Icons.password,
-                        input: TextInputType.number),
-                    SizedBox(
-                      height: 20,
                     ),
-                  ],
+                  )),
+              Positioned(
+                bottom: size.height * Whitebackgroudheight,
+                child: Container(
+                    height: size.height * 0.05,
+                    width: size.width / 2,
+                    decoration: BoxDecoration(
+                        color: Color(0xff120879),
+                        borderRadius: BorderRadius.only(
+                          bottomLeft: Radius.circular(26),
+                        ))),
+              ),
+              Positioned(
+                bottom: size.height * (Whitebackgroudheight + 0.025),
+                left: (size.width -
+                        size.height * (1 - Whitebackgroudheight - 0.15)) /
+                    2,
+                child: Image(
+                  width: size.height * (1 - Whitebackgroudheight - 0.15),
+                  height: size.height * (1 - Whitebackgroudheight - 0.15),
+                  image: AssetImage("assets/verifcode.PNG"),
                 ),
               ),
-            ),
-            Positioned(
-                bottom: size.height / 5,
+              Positioned(
+                bottom: size.height * (Whitebackgroudheight - 0.07),
+                left: size.width / 12,
+                child: Text(
+                  "Verification Code",
+                  style: TextStyle(fontSize: 30),
+                ),
+              ),
+              Positioned(
+                  bottom: size.height * (Whitebackgroudheight - 0.1),
+                  left: size.width / 10,
+                  child: Text("Add text here")),
+              Positioned(
+                bottom: size.height / 3,
                 width: size.width / 1.2,
                 left: size.width / 12,
-                child: Button(
-                  text: "Verify Now",
-                  color: Color(0xfffb716a),
-                  onClick: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => ForgetPassPage()));
-                  },
-                )),
-            Positioned(
-              bottom: size.height / 6.5,
-              left: size.width / 2 - 30,
-              child: Text(
-                "Resend",
-                style: TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold,
-                  color: Color(0xFFFB716A),
+                child: Form(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      textInput("****", Icons.password,
+                          input: TextInputType.number),
+                      SizedBox(
+                        height: 20,
+                      ),
+                    ],
+                  ),
                 ),
               ),
-            ),
-          ],
+              Positioned(
+                  bottom: size.height / 5,
+                  width: size.width / 1.2,
+                  left: size.width / 12,
+                  child: Button(
+                    text: "Verify Now",
+                    color: Color(0xfffb716a),
+                    onClick: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => ForgetPassPage()));
+                    },
+                  )),
+              Positioned(
+                bottom: size.height / 6.5,
+                left: size.width / 2 - 30,
+                child: Text(
+                  "Resend",
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    color: Color(0xFFFB716A),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
